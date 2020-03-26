bd69bd6b5572274279cd62c5af1f36a39713c4a3
Nwokochahannah <hannahsopuruchi@gmail.com>
2020-03-26 23:35:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let name = "Hannah";
+
+let courses = ["HTML" , "CSS" , "JavaScript"];
+
+    console.log("My name is: " + name);
+
+    console.log("My courses are: " + courses);
+
+const number_of_courses = [];
+
+for (let k = 0; k <= 200; k++) {
+    if (k%2 === 1) {
+        number_of_courses.push(k);
+    }
+}
+console.log(number_of_courses);
